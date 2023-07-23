59f804b9e54b8d8f3a19d154fb1527e72db3fc99
YouYou314 <you.you@my.jcu.edu.au>
2023-07-23 23:39:21 +0800

    write a Menu for score

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,3 +1,4 @@
+import random
 Menu = input("(G)et a valid score"
              "(P)rint result"
              "(S)how stars"
@@ -6,29 +7,39 @@ Menu = input("(G)et a valid score"
 
 def main():
     print(Menu)
-    choice = Menu
+    score = float(input("Enter score: "))
+    score = get_valid_score(score)
+    choice = input("Your choice: ").upper()
     while choice != "Q":
         if choice == "G":
-            score = get_valid_score()
+            score = float(input("Enter score: "))
+            score = get_valid_score(score)
         elif choice == "P":
             result = get_score_result(score)
+            print(f"Result: {result}")
         elif choice == "S":
             show_stars(score)
         else:
             print("Invalid input error message")
         print(Menu)
-        choice = Menu
+        choice = input("Your choice: ").upper()
+    else:
+        print("Farewell.")
+
+    random_score = random.uniform(0, 100)
+    result = get_score_result(random_score)
+    print(f"Random Score: {random_score:.2f};\n{result}")
 
 
-def get_valid_score():
-    score = float(input("Enter score: "))
+def get_valid_score(score):
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
     return score
 
 
 def get_score_result(score):
-    if score < 0 or score > 100:
-        return "Invalid score"
-    elif score > 90:
+    if score > 90:
         return "Excellent"
     elif score > 50:
         return "Passable"
