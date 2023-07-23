b4e004bfb2924333ba66f1e891e44e66e637d907
YouYou314 <you.you@my.jcu.edu.au>
2023-07-23 20:28:32 +0800

    add two more functions for converting Celsius to Fahrenheit and vice versa

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -7,17 +7,27 @@ Q - Quit"""
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
+            fahrenheit = celsius_to_fahrenheit(celsius)
             print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = fahrenheit_to_celsius(fahrenheit)
             print(f"Result: {celsius:.2f} F")
         else:
             print("Invalid option")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
 
 
 main()
