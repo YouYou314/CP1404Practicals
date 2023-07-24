c3072b291fa7f69e029255164e854b68e84961a0
YouYou314 <you.you@my.jcu.edu.au>
2023-07-24 18:15:56 +0800

    Write code that opens "name.txt" and reads the name (as above) then prints

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,3 +4,8 @@ name = input("Please enter your name: ")
 with open("name.txt", "w") as file:
     file.write(name)
 
+# Step 2
+with open("name.txt", "r") as file:
+    name = file.read()
+
+print("Your name is", name)
