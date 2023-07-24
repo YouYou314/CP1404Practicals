976089f924c722f35e71161fe7124ec3ea09a0af
YouYou314 <you.you@my.jcu.edu.au>
2023-07-24 18:18:10 +0800

    Write code that opens "numbers.txt", reads only the first two numbers and adds them together then prints the result

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,3 +9,14 @@ with open("name.txt", "r") as file:
     name = file.read()
 
 print("Your name is", name)
+
+# Step 3
+with open("numbers.txt", "w") as file:
+    file.write("17\n42\n400")
+
+with open("numbers.txt", "r") as file:
+    first_number = int(file.readline().strip())
+    second_number = int(file.readline().strip())
+
+result = first_number + second_number
+print("The result is:", result)
