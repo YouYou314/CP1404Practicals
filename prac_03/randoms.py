4ab013727f31cd3d8c959f1b3dcd5e0c213251cb
YouYou314 <you.you@my.jcu.edu.au>
2023-07-24 17:11:15 +0800

    write a code to produce a random number between 1 and 100 inclusive.

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,4 +1,7 @@
-"""I saw a number, the smallest number I could have seen is 5,the largest is 19."""
-"""I saw a number, the smallest number is 3, the largest is 9. The line 2 couldn't produced 4"""
-"""I saw a number which is not integer, the smallest number is 2.5, the largest number is
- 5.5 """
+"""I saw a number, the smallest number I could have seen is 5,the largest is 19.
+I saw a number, the smallest number is 3, the largest is 9. The line 2 couldn't produce 4.
+I saw a number which is not integer, the smallest number is 2.5, the largest number is 5.5. """
+
+import random
+
+print(random.randint(1, 100))
