8df33b0a6c8c00afbbb426606a41d3c22ff943a6
YouYou314 <you.you@my.jcu.edu.au>
2023-07-24 17:04:17 +0800

    write code in console and get the findings.

---
+++ prac_03/randoms.py
@@ -0,0 +1,4 @@
+"""I saw a number, the smallest number I could have seen is 5,the largest is 19."""
+"""I saw a number, the smallest number is 3, the largest is 9. The line 2 couldn't produced 4"""
+"""I saw a number which is not integer, the smallest number is 2.5, the largest number is
+ 5.5 """
