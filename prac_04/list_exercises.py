5c641f068dc3e840cb8982347f12bde50cff8163
YouYou314 <you.you@my.jcu.edu.au>
2023-08-06 23:14:26 +0800

    Ask the user for their username. If the username is in the above list of authorised users, print "Access granted", otherwise print "Access denied".

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -15,3 +15,13 @@ def main():
 
 
 main()
+
+# Task 2
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
