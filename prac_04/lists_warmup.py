13ef7a95991fda9e7da6f34571dc92394af4a170
YouYou314 <you.you@my.jcu.edu.au>
2023-08-06 22:15:51 +0800

    a warm-up prac.

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,20 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers[0]
+# answer: 3
+numbers[-1]
+# answer: 2
+numbers[3]
+# answer: 1
+numbers[:-1]
+# answer: [3, 1, 4, 5, 9]
+numbers[3:4]
+# answer: [1]
+5 in numbers
+# answer: Ture
+7 in numbers
+# answer: False
+"3" in numbers
+# answer: False
+numbers + [6, 5, 3]
+# answer: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
