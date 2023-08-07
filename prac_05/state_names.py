cf27afe53293057e7905e75fba2672a1d2afc145
YouYou314 <you.you@my.jcu.edu.au>
2023-08-07 17:59:15 +0800

    Change this to use exceptions and the "Easier to Ask Forgiveness than Permission" (EAFP) approach.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,15 +9,22 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    upper_state_code = state_code.upper()  # Convert user input to uppercase
-    if upper_state_code in CODE_TO_NAME:
-        print(upper_state_code, "is", CODE_TO_NAME[upper_state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ")
-
+# state_code = input("Enter short state: ")
+# while state_code != "":
+#     upper_state_code = state_code.upper()  # Convert user input to uppercase
+#     if upper_state_code in CODE_TO_NAME:
+#         print(upper_state_code, "is", CODE_TO_NAME[upper_state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ")
+#
 for state_code, state_name in CODE_TO_NAME.items():
     print(f"{state_code:<4} is {state_name}")
 
+state_code = input("Enter short state: ")
+while state_code != "":
+    try:
+        print(f"{state_code.upper()} is {state_name}")
+    except KeyError:
+        print("Invalid short state")
+    state_code = input("Enter short state: ")
