cc12f49799a0341564636cac73af42e5533777a4
YouYou314 <you.you@my.jcu.edu.au>
2023-08-13 21:44:18 +0800

    write a main code, include a function and a loop.

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -11,3 +11,18 @@ COLOR_TO_HEX = {
     "BLUEVIOLET": "#8A2BE2",
 }
 
+
+def lookup_color_code(color_name):
+    return COLOR_TO_HEX.get(color_name.upper())
+
+
+color_name = input("Enter a color name: ")
+
+while color_name != "":
+    color_code = lookup_color_code(color_name)
+    if color_code:
+        print(f"{color_name.upper()} color code is {color_code}")
+    else:
+        print("Invalid color name")
+
+    color_name = input("Enter a color name: ")
