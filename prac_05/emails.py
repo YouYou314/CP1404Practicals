def7fe58d9f14fd19aa94a375329a2f3eec22da2
YouYou314 <you.you@my.jcu.edu.au>
2023-08-13 22:43:49 +0800

    write the main function, ask user's email, and name.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,4 +5,20 @@ def extract_name(email):
     return name
 
 
+def main():
+    user_data = {}
+
+    while True:
+        email = input("Email: ")
+        if not email:
+            break
+
+        name = extract_name(email)
+        correct_name = input(f"Is your name {name}? (Y/n) ").strip().lower()
+
+        if correct_name == '' or correct_name == 'y':
+            user_data[email] = name
+        else:
+            name = input("Name: ")
+            user_data[email] = name
 
