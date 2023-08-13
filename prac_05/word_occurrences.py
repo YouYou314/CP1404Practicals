3d1789b3077c1af0db72cc7debeac72a77d8899a
YouYou314 <you.you@my.jcu.edu.au>
2023-08-13 22:23:21 +0800

    Count occurrences of each word

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,3 +2,10 @@ def main():
     user_input = input("Enter a string: ")
     words = user_input.split()
     word_counts = {}
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+
