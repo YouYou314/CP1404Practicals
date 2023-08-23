6358dcc2960c93303a1e709250121b6b5a7dcb0f
YouYou314 <you.you@my.jcu.edu.au>
2023-08-24 01:18:19 +0800

    correct the problem guitar = [] to guitars = [].

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,7 +2,7 @@ from prac_06.guitar import Guitar
 
 
 def main():
-    guitar = []
+    guitars = []
 
     print("My guitars!")
     name = input("Name: ")
@@ -14,12 +14,18 @@ def main():
         print(guitar_add, "added.")
         name = input("Name: ")
 
-    guitar.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitar.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     if guitars:
         print("These are my guitars:")
         for i, guitar in enumerate(guitars, 1):
-            vintage_srting = ""
+            vintage_string = ""
             if guitar.is_vintage():
-                vintage_srting = " (vintage)"
+                vintage_string = " (vintage)"
+            print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i, guitar, vintage_string))
+    else:
+        print("No guitars :( Quick, go and buy one!")
+
+
+main()
