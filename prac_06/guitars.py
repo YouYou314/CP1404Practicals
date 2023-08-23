d9b643acea5a83a974b965d0ee5f3ea6b5ec10e5
YouYou314 <you.you@my.jcu.edu.au>
2023-08-24 01:13:44 +0800

    the use of the format method and numbered placeholders

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -16,3 +16,10 @@ def main():
 
     guitar.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitar.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_srting = ""
+            if guitar.is_vintage():
+                vintage_srting = " (vintage)"
