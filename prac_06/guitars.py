2c60dda4bdc294aebbb7e8dadf70d6c4a16784b5
YouYou314 <you.you@my.jcu.edu.au>
2023-08-24 01:24:37 +0800

    add the $ in the line 11.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -8,7 +8,7 @@ def main():
     name = input("Name: ")
     while name != "":
         year = int(input("Year: "))
-        cost = float(input("Cost: "))
+        cost = float(input("Cost: $"))
         guitar_add = Guitar(name, year, cost)
         guitars.append(guitar_add)
         print(guitar_add, "added.")
