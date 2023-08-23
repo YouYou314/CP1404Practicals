985a973aac7de6db8ef40da97184c0e0cc7b4093
YouYou314 <you.you@my.jcu.edu.au>
2023-08-24 01:10:54 +0800

    write guitar program using Guitar class.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,3 +1,18 @@
 from prac_06.guitar import Guitar
 
 
+def main():
+    guitar = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitar_add = Guitar(name, year, cost)
+        guitar.append(guitar_add)
+        print(guitar_add, "added.")
+        name = input("Name: ")
+
+    guitar.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitar.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
