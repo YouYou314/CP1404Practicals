79192b2500fdf093df29de2b97b5db7970d1af37
YouYou314 <you.you@my.jcu.edu.au>
2023-08-24 00:35:27 +0800

    add constants

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,3 +1,7 @@
+CURRENT_YEAR = 2023
+VINTAGE_AGE = 50
+
+
 class Guitar:
 
     def __init__(self, name="", year=0, cost=0):
@@ -8,3 +12,6 @@ class Guitar:
     def __str__(self):
         """Return a string representation of a Guitar."""
         return f"{self.name} {self.year}: ${self.cost:,.2f}"
+
+    def get_age(self):
+        return
