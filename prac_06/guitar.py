ba31bbdf6349dcc15c2a728cb65ff6ebe9478f59
YouYou314 <you.you@my.jcu.edu.au>
2023-08-23 19:04:27 +0800

    Return a string representation of a Guitar.

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,3 +4,7 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of a Guitar."""
+        return f"{self.name} {self.year}: ${self.cost:,.2f}"
