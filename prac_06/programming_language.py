8532e98e8765a77fb52e6a3246d677d2af4fc223
YouYou314 <you.you@my.jcu.edu.au>
2023-08-23 17:33:31 +0800

    call the class and create the field and set them to the parameters passed in

---
+++ prac_06/programming_language.py
@@ -0,0 +1,8 @@
+class ProgrammingLanguage:
+
+    def __int__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
