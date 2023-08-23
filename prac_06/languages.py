317ceeb1cbe51f55480fd3e8987fdf29ad489dde
YouYou314 <you.you@my.jcu.edu.au>
2023-08-23 18:56:51 +0800

    add the list to produce the output.

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,8 +1,16 @@
-from prac_06.programming_language import import
+from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    print(python)
+
+    language = [python, ruby, visual_basic]
+    print("The dynamically typed language are: ")
+    for language in language:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
