3466cfe244a59994c910b32eef214c9609bba7a8
YouYou314 <you.you@my.jcu.edu.au>
2023-08-23 18:48:25 +0800

    add the list

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -11,3 +11,13 @@ class ProgrammingLanguage:
 
     def is_dynamic(self):
         return self.typing == "Dynamic"
+
+    def run_tests():
+        python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+        ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+        visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+        language = [python, ruby, visual_basic]
+        print(python)
+
+
