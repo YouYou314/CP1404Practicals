d4664e8c861d3fba835f72c2b14bf1130359ff6c
YouYou314 <you.you@my.jcu.edu.au>
2023-08-24 01:00:39 +0800

    print the data

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -11,4 +11,10 @@ def run_tests():
     guitar = Guitar(name, year, cost)
     other = Guitar("Another Guitar", 2013, 1888.78)
 
+    print(f"{guitar.name} get_age() - Expected {100}. Got {guitar.get_age()}")
+    print(f"{other.name} get_age() - Expected {9}. Got {other.get_age()}")
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
 
+
+run_tests()
